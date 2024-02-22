275d09899909a148fab430456299ca7092400437
Seyyed Morteza Moosavi <smmoosavi@users.noreply.github.com>
2024-02-22 12:50:03 +0330

    export all generated types (#6)

--- packages/pkl-ts-parser/src/pkl-ts-parser.ts
+++ packages/pkl-ts-parser/src/pkl-ts-parser.ts
@@ -1,4 +1,4 @@
-export { PklTsParser } from './antlr4/ts/PklTsParser';
-export { PklTsLexer } from './antlr4/ts/PklTsLexer';
-export { PklTsParserListener } from './antlr4/ts/PklTsParserListener';
-export { PklTsParserVisitor } from './antlr4/ts/PklTsParserVisitor';
+export * from './antlr4/ts/PklTsParser';
+export * from './antlr4/ts/PklTsLexer';
+export * from './antlr4/ts/PklTsParserListener';
+export * from './antlr4/ts/PklTsParserVisitor';
